b5d2161b2e629d88e0ecb341401185b81c4135cc
jiawen <jiawenzhu9611@gmail.com>
2020-06-07 16:19:45 +0800

    add project 2

--- project_2/camera_calibration.py
+++ project_2/camera_calibration.py
@@ -19,29 +19,19 @@ def camera_calibration(pt_3d, matches):
     #return R: rotation matrix, np array (3,3)
     #return c: camera center, np array (4,)
 
-    points_num = matches.shape[0]
-    a = []
-    b = []
-
-    for n in range(points_num):
-        x = pt_3d[n,0]
-        y = pt_3d[n,1]
-        z = pt_3d[n,2]
-        u = matches[n,0]
-        v = matches[n,1]
+    a, b = [], []
+    for n in range(matches.shape[0]):
+        x, y, z, u, v = pt_3d[n,0], pt_3d[n,1], pt_3d[n,2], matches[n,0], matches[n,1]
         a.append([x,y,z,1,0,0,0,0, -u*x, -u*y, -u*z])
+        a.append([0, 0, 0, 0, x, y, z, 1, -v * x, -v * y, -v * z])
         b.append([u])
-        a.append([0,0,0,0,x,y,z,1, -v*x, -v*y, -v*z])
         b.append([v])
 
-    A = np.mat(a)
-    B = np.mat(b)
+    A, B = np.mat(a), np.mat(b)
     M = np.dot(np.linalg.inv(np.dot(A.T, A)), np.dot(A.T, B))
-    M = np.array(M.T)
-    M = np.append(M,[1])
-    P = np.reshape(M,(3,4))
-    R, K = np.linalg.qr(P)
+    P = np.reshape(np.append(np.array(M.T),[1]),(3,4))
     c = np.dot(np.linalg.inv(np.dot(-P[:, 0:3].T, -P[:, 0:3])), np.dot(-P[:, 0:3].T, P[:, 3]))
+    R, K = np.linalg.qr(P)
 
     # <YOUR CODE>
 
